cefbb8789e2f5868e955d1452fe1e0ef7da88bf4
Elias Mendoza <eliasmm@gmail.com>
2025-12-03 16:01:47 -0600

    adding useEffect and UseReducer in the first project

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,15 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import React, { useState } from "react";
+import { useEffect, useState, useReducer } from "react";
+import { button } from 'reactstrap';
 import RecipeForm from "./components/RecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeModal from "./components/RecipeModal";
+import { Button } from "reactstrap";
 
 function App() {
   const [recipe, setRecipe] = useState([]);
   const [loader, setLoader] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
   const [modal, setModal] = useState(false);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return { count: state.count + 1 }
+      case "DECREMENT":
+        return { count: state.count - 1 }
+      case "RESET":
+        return { count: 0 }
+
+
+
+    }
+  }
+  const initialState = {
+    count: 0
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggle = () => setModal(!modal);
 
@@ -21,9 +40,15 @@ function App() {
       setLoader(false);
     }, 5000);
   };
-
+  useEffect(() => {
+    console.log("recipe is added");
+  }, [recipe]);
   return (
     <>
+      <p>Count:{state.count}</p>
+      <button className="btn btn-primary" onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button className="btn btn-secondary" onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
+      <Button className="btn btn-danger" onClick={() => dispatch({ type: "RESET" })}>Reset</Button>
       <RecipeForm addRecipe={addRecipe} loader={loader} />
 
       <RecipeList
